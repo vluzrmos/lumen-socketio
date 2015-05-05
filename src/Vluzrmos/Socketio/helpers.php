ec1aa2d07783afbba733ea2f53ea4da6df5d4bca
Vagner do Carmo <vluzrmos@gmail.com>
2015-05-04 22:48:05 -0300

    Better performance at publish helper

--- src/Vluzrmos/Socketio/helpers.php
+++ src/Vluzrmos/Socketio/helpers.php
@@ -1,7 +1,8 @@
 <?php
 
 if(!function_exists('publish')){
-    function publish($channel, $event, $message){
-        return call_user_func_array([app('broadcast'), 'publish'], func_get_args());
+    function publish($channel, $event, $message)
+    {
+        return app('broadcast')->publish($channel, $event, $message);
     }
 }
